caf771fcab7ea088277e436e7063ecc2d458e124
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-12-22 01:57:19 -0400

    feat: applying cors on the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Task Manager')
     .setDescription('Task manager API document')
