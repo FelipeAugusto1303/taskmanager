5f0dd4e651e6e40d4bb31b642b0d15501a943f45
FelipeAugusto1303 <felipe.augusto.1303@gmail.com>
2023-12-22 02:01:04 -0400

    feat: updating home component

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/shared/task/task';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  taskList: Task[] = [];
+  constructor(private service: TaskService) {}
 
   ngOnInit(): void {
+    this.service.listTasks().subscribe((list) => {
+      this.taskList = list;
+    });
   }
 
+  updateTaskList() {
+    this.service.listTasks().subscribe((list) => {
+      this.taskList = list;
+    });
+  }
 }
